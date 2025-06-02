cf48178f06615a1dd38856d0d541a4651124d6fc
neon-izm <jj.y0sh1da@gmail.com>
2025-06-03 00:26:28 +0900

    一旦読み込みが動いた

--- Assets/Scripts/Pages/CharacterColorChangePage/CharacterColorChangePageLifecycle.cs
+++ Assets/Scripts/Pages/CharacterColorChangePage/CharacterColorChangePageLifecycle.cs
@@ -24,10 +24,23 @@ public class CharacterColorChangePageLifecycle : LifecyclePageBase
         _publisher = publisher;
         _guid = guidCounterService;
         _userSettingsUseCase = userSettingsUseCase;
+       
+        _userSettingsUseCase.RightHandColor.Subscribe(x =>
+        {
+            _view.SetRightHandColor(x);
+        });
+        _userSettingsUseCase.LeftHandColor.Subscribe(x =>
+        {
+            _view.SetLeftHandColor(x);
+        });
+        _userSettingsUseCase.RightFootColor.Subscribe(x=>_view.SetRightFootColor(x));
+        _userSettingsUseCase.LeftFootColor.Subscribe(x=>_view.SetLeftFootColor(x));
+        
     }
 
     protected override UniTask WillPushEnterAsync(CancellationToken cancellationToken)
     {
+        Debug.Log($"WillPushEnterAsync called RightHandColor:{_userSettingsUseCase.RightHandColor.Value}");
         
         // 現在の設定値を画面に反映
         _view.SetRightHandColor(_userSettingsUseCase.RightHandColor.Value);
@@ -44,30 +57,30 @@ public class CharacterColorChangePageLifecycle : LifecyclePageBase
 
         // 戻るボタン
         _view.OnClickReturnButton.Subscribe(_ => _publisher.SendPopEvent());
-        
+       
         // 各色変更イベントをUserSettingsUseCaseに連携
         _view.OnRightHandColorChanged.Subscribe(color => _userSettingsUseCase.UpdateRightHandColor(color));
         _view.OnLeftHandColorChanged.Subscribe(color => _userSettingsUseCase.UpdateLeftHandColor(color));
         _view.OnRightFootColorChanged.Subscribe(color => _userSettingsUseCase.UpdateRightFootColor(color));
         _view.OnLeftFootColorChanged.Subscribe(color => _userSettingsUseCase.UpdateLeftFootColor(color));
-        
+       
         /*
         // リセットボタン（オプション）
-        _view.OnClickResetButton.Subscribe(_ => 
+        _view.OnClickResetButton.Subscribe(_ =>
         {
             _userSettingsUseCase.UpdateRightHandColor(Color.white);
             _userSettingsUseCase.UpdateLeftHandColor(Color.white);
             _userSettingsUseCase.UpdateRightFootColor(Color.white);
             _userSettingsUseCase.UpdateLeftFootColor(Color.white);
-            
+
             // UI側も更新
             _view.SetRightHandColor(Color.white);
             _view.SetLeftHandColor(Color.white);
             _view.SetRightFootColor(Color.white);
             _view.SetLeftFootColor(Color.white);
-            
+
         });
         */
-        
+
     }
 } 
--- Assets/Scripts/Pages/CharacterColorChangePage/CharacterColorChangePageView.cs
+++ Assets/Scripts/Pages/CharacterColorChangePage/CharacterColorChangePageView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using Demo.Subsystem.GUIComponents.TabGroup;
 using ScreenSystem.Page;
 using UniRx;
@@ -22,10 +23,10 @@ public class CharacterColorChangePageView : PageViewBase
     };
 
     // 各部位の色変更時のイベント
-    private readonly Subject<Color> _rightHandColorChanged = new Subject<Color>();
-    private readonly Subject<Color> _leftHandColorChanged = new Subject<Color>();
-    private readonly Subject<Color> _rightFootColorChanged = new Subject<Color>();
-    private readonly Subject<Color> _leftFootColorChanged = new Subject<Color>();
+    private ReactiveProperty<Color> _rightHandColorChanged = new ReactiveProperty<Color>();
+    private ReactiveProperty<Color> _leftHandColorChanged = new ReactiveProperty<Color>();
+    private ReactiveProperty<Color> _rightFootColorChanged = new ReactiveProperty<Color>();
+    private ReactiveProperty<Color> _leftFootColorChanged = new ReactiveProperty<Color>();
 
     // イベント公開プロパティ
     public IObservable<Unit> OnClickReturnButton => _returnButton.OnClickAsObservable();
@@ -42,6 +43,7 @@ public class CharacterColorChangePageView : PageViewBase
         itemSetTabGroup.OnTabLoaded
             .Subscribe(x =>
             {
+                
                 Debug.Log(x.Sheet.name + " is loaded");
                 
                 var itemSetSheet = (CharacterColorSheet)x.Sheet;
@@ -50,41 +52,42 @@ public class CharacterColorChangePageView : PageViewBase
                 {
                     case 0:
                         //右手
-                        itemSetSheet.SetColor(OnRightHandColorChanged.ToReadOnlyReactiveProperty().Value);
+                        Debug.Log($"Right Hand Color{_rightHandColorChanged.Value} ");
+                        itemSetSheet.SetColor(_rightHandColorChanged.Value);
                         itemSetSheet._targetColor.Subscribe(
                             x =>
                             {
-                                _rightHandColorChanged.OnNext(x);
+                                _rightHandColorChanged.Value = x;
                             }
                             ).AddTo(this);
                         break;
                     case 1:
                         //左手
-                        itemSetSheet.SetColor(OnLeftHandColorChanged.ToReadOnlyReactiveProperty().Value);
+                        itemSetSheet.SetColor(_leftHandColorChanged.Value);
                         itemSetSheet._targetColor.Subscribe(
                             x =>
                             {
-                                _leftHandColorChanged.OnNext(x);
+                                _leftHandColorChanged.Value =x;
                             }
                         ).AddTo(this);
                         break;
                     case 2:
                         //右足
-                        itemSetSheet.SetColor(OnRightFootColorChanged.ToReadOnlyReactiveProperty().Value);
+                        itemSetSheet.SetColor(_rightFootColorChanged.Value);
                         itemSetSheet._targetColor.Subscribe(
                             x =>
                             {
-                                _rightFootColorChanged.OnNext(x);
+                                _rightFootColorChanged.Value= x;
                             }
                         ).AddTo(this);
                         break;
                     case 3:
                         //左足
-                        itemSetSheet.SetColor(OnLeftFootColorChanged.ToReadOnlyReactiveProperty().Value);
+                        itemSetSheet.SetColor(_leftFootColorChanged.ToReactiveProperty().Value);
                         itemSetSheet._targetColor.Subscribe(
                             x =>
                             {
-                                _leftFootColorChanged.OnNext(x);
+                                _leftFootColorChanged.Value = x;
                             }
                         ).AddTo(this);
                         break;
@@ -95,60 +98,31 @@ public class CharacterColorChangePageView : PageViewBase
             })
             .AddTo(this);
 
+        Debug.Log("CharacterColorChangePageView created");
         await itemSetTabGroup.InitializeAsync();
-        // 簡易的な色選択実装（ランダムカラー）
-        // 実際の実装では、ColorPickerやパレットUIを使用
-        /*
-        _rightHandColorButton.OnClickAsObservable().Subscribe(_ => 
-        {
-            var randomColor = _presetColors[UnityEngine.Random.Range(0, _presetColors.Length)];
-            SetRightHandColor(randomColor);
-            _rightHandColorChanged.OnNext(randomColor);
-        });
         
-        _leftHandColorButton.OnClickAsObservable().Subscribe(_ => 
-        {
-            var randomColor = _presetColors[UnityEngine.Random.Range(0, _presetColors.Length)];
-            SetLeftHandColor(randomColor);
-            _leftHandColorChanged.OnNext(randomColor);
-        });
-        
-        _rightFootColorButton.OnClickAsObservable().Subscribe(_ => 
-        {
-            var randomColor = _presetColors[UnityEngine.Random.Range(0, _presetColors.Length)];
-            SetRightFootColor(randomColor);
-            _rightFootColorChanged.OnNext(randomColor);
-        });
-        
-        _leftFootColorButton.OnClickAsObservable().Subscribe(_ => 
-        {
-            var randomColor = _presetColors[UnityEngine.Random.Range(0, _presetColors.Length)];
-            SetLeftFootColor(randomColor);
-            _leftFootColorChanged.OnNext(randomColor);
-        });
-        */
     }
 
    
     // 各部位の色を設定
     public void SetRightHandColor(Color color)
     {
-        _rightHandColorChanged.OnNext(color);
+        _rightHandColorChanged.SetValueAndForceNotify(color);
     }
 
     public void SetLeftHandColor(Color color)
     {
-        _leftHandColorChanged.OnNext(color);
+        _leftHandColorChanged.SetValueAndForceNotify(color);
     }
 
     public void SetRightFootColor(Color color)
     {
-        _rightFootColorChanged.OnNext(color);
+        _rightFootColorChanged.SetValueAndForceNotify(color);
     }
 
     public void SetLeftFootColor(Color color)
     {
-        _leftFootColorChanged.OnNext(color);
+        _leftFootColorChanged.SetValueAndForceNotify(color);
     }
     private void OnDestroy()
     {
