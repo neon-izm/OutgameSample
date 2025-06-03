040474b32aff7297822fa9b9504ed3de8770ffd8
neon-izm <jj.y0sh1da@gmail.com>
2025-06-04 01:44:52 +0900

    Footer scaling

---
+++ Assets/Scripts/Pages/Footer/FooterLifetimeScope.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class FooterLifetimeScope : LifetimeScope
+{
+    [SerializeField] private FooterView _view;
+
+    protected override void Configure(IContainerBuilder builder)
+    {
+        Debug.Log("FooterLifetimeScope Configure");
+        //builder.Register<GuidCounter>(Lifetime.Singleton);
+        builder.RegisterEntryPoint<FooterLifecycle>(Lifetime.Singleton);
+
+        builder.RegisterComponent(_view);
+        
+    }
+}
